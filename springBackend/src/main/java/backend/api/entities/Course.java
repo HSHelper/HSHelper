7286aa83d46d38c34dc95945e835932dbb9495ca
Atos1337 <victor.samoilov.2002@mail.ru>
2021-03-19 15:23:43 +0300

    Create defaultPartition relationship between course and partition

--- springBackend/src/main/java/backend/api/entities/Course.java
+++ springBackend/src/main/java/backend/api/entities/Course.java
@@ -6,24 +6,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import java.util.Set;
 
 @Entity
-public class Partition {
+public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
 
-    @OneToMany(mappedBy = "partition")
-    private Set<UserToPartition> users;
-
     @ManyToOne
-    @JoinColumn(name = "group_id", nullable = false)
-    private Group group;
-
-    @OneToMany(mappedBy = "defaultPartition")
-    Set<Course> coursesWithThisDefaultPartition;
+    @JoinColumn(name = "default_partition_id")
+    private Partition defaultPartition;
 }
