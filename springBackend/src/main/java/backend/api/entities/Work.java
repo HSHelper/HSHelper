c3d71fc0e9aaae766e3b39f2962ddde26c501f7a
Atos1337 <victor.samoilov.2002@mail.ru>
2021-03-19 16:02:15 +0300

    Create Work Table and create relationship between work and coursePart

---
+++ springBackend/src/main/java/backend/api/entities/Work.java
@@ -0,0 +1,31 @@
+package backend.api.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
+public class Work {
+    public enum WorkType {
+        HOMEWORK,
+        CONTROLWORK
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String description;
+    private Date deadline;
+    private double weight;
+    private double block;
+    private WorkType workType;
+
+    @ManyToOne
+    @JoinColumn(name = "course_part_id", nullable = false)
+    private CoursePart coursePart;
+}
