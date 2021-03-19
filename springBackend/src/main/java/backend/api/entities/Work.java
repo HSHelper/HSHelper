41dc12a5f063362eb1d51de76d97cfa85fc356dc
Atos1337 <victor.samoilov.2002@mail.ru>
2021-03-19 16:09:15 +0300

    Create UserWork relationship

--- springBackend/src/main/java/backend/api/entities/Work.java
+++ springBackend/src/main/java/backend/api/entities/Work.java
@@ -6,7 +6,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import java.util.Date;
+import java.util.Set;
 
 @Entity
 public class Work {
@@ -28,4 +30,7 @@ public class Work {
     @ManyToOne
     @JoinColumn(name = "course_part_id", nullable = false)
     private CoursePart coursePart;
+
+    @OneToMany(mappedBy = "work")
+    private Set<UserWork> users;
 }
