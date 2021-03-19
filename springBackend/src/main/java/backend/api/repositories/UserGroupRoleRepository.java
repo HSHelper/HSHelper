2768b4c42002a4da035fcc9131648d63f331b40e
Atos1337 <victor.samoilov.2002@mail.ru>
2021-03-19 21:07:12 +0300

    Create deleteByUser in all repositories related on User and create deleteById in UserController

--- springBackend/src/main/java/backend/api/repositories/UserGroupRoleRepository.java
+++ springBackend/src/main/java/backend/api/repositories/UserGroupRoleRepository.java
@@ -1,8 +1,10 @@
 package backend.api.repositories;
 
+import backend.api.entities.User;
 import backend.api.entities.UserGroupRole;
 import backend.api.keys.UserGroupRoleKey;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserGroupRoleRepository extends CrudRepository<UserGroupRole, UserGroupRoleKey> {
+    void deleteByUser(User user);
 }
