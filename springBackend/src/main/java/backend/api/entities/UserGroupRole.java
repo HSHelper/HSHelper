06f9f9a719ff6e9866a568745b745dc0e4f183bc
Atos1337 <victor.samoilov.2002@mail.ru>
2021-03-19 15:14:43 +0300

    Create UserToPartition relationship

---
+++ springBackend/src/main/java/backend/api/entities/UserGroupRole.java
@@ -0,0 +1,61 @@
+package backend.api.entities;
+
+import backend.api.keys.UserGroupRoleKey;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+
+@Entity
+public class UserGroupRole {
+    @EmbeddedId
+    private UserGroupRoleKey id;
+
+    @ManyToOne
+    @MapsId("userId")
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @MapsId("groupId")
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    public UserGroupRoleKey getId() {
+        return id;
+    }
+
+    public void setId(UserGroupRoleKey id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
