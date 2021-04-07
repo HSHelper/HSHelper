335ddafbdac7034d4f3e29bb8f58e6ba0b1baa58
kirill <turovkv@yandex.ru>
2021-04-07 10:52:14 +0300

    Create empty login activity

---
+++ androidApp/app/src/main/java/ru/hsHelper/data/LoginDataSource.kt
@@ -0,0 +1,27 @@
+package ru.hsHelper.data
+
+import ru.hsHelper.data.model.LoggedInUser
+import java.io.IOException
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+class LoginDataSource {
+
+    fun login(username: String, password: String): Result<LoggedInUser> {
+        try {
+            // TODO: handle loggedInUser authentication
+            if (username.length % 2 == 0) {
+                throw IOException("kek")
+            }
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
+            return Result.Success(fakeUser)
+        } catch (e: Throwable) {
+            return Result.Error(IOException("Error logging in", e))
+        }
+    }
+
+    fun logout() {
+        // TODO: revoke authentication
+    }
+}
