ba8e020782a640d11352ab61a2aced27caf8daf6
Atos1337 <victor.samoilov.2002@mail.ru>
2021-04-07 22:24:25 +0300

    Create RoleService

--- server/src/main/java/ru/hsHelper/api/repositories/PermissionsRepository.java
+++ server/src/main/java/ru/hsHelper/api/repositories/PermissionsRepository.java
@@ -4,7 +4,9 @@ import ru.hsHelper.api.entities.Permissions;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface PermissionsRepository extends CrudRepository<Permissions, Long> {
     Optional<Permissions> findByPermissionType(Permissions.PermissionType permissionType);
+    Set<Permissions> findByIdIn(Set<Long> ids);
 }
---
+++ server/src/main/java/ru/hsHelper/api/services/RoleService.java
@@ -0,0 +1,15 @@
+package ru.hsHelper.api.services;
+
+import ru.hsHelper.api.entities.Permissions;
+import ru.hsHelper.api.entities.Role;
+
+import java.util.Set;
+
+public interface RoleService {
+    Role createRole(Role role);
+    void deleteRole(long id);
+    Role getRoleById(long id);
+    Set<Role> getRoleByRoleType(Role.RoleType roleType);
+    Role addPermissions(long roleId, Set<Long> permissionsIds);
+    Role deletePermissions(long roleId, Set<Long> permissionIds);
+}
