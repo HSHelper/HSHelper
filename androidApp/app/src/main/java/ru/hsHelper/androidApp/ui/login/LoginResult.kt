2adcb735826deb1ab4a6122a5ba624d1c0ea85c5
Roman Venediktov <e2e4b6b7@gmail.com>
2021-04-09 11:14:40 +0300

    Add StringRes annotation

--- androidApp/app/src/main/java/ru/hsHelper/androidApp/ui/login/LoginResult.kt
+++ androidApp/app/src/main/java/ru/hsHelper/androidApp/ui/login/LoginResult.kt
@@ -1,8 +1,9 @@
 package ru.hsHelper.androidApp.ui.login
 
+import androidx.annotation.StringRes
 import com.google.firebase.auth.FirebaseUser
 
 sealed class LoginResult {
     data class Success(val success: FirebaseUser) : LoginResult()
-    data class Error(val error: Int) : LoginResult()
+    data class Error(@StringRes val error: Int) : LoginResult()
 }
