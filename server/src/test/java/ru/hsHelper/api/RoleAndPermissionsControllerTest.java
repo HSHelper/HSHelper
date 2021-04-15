ad12106d41c16a28adae20ee364dbc8cff47296c
Atos1337 <victor.samoilov.2002@mail.ru>
2021-04-15 11:32:00 +0300

    Refactor basicTest for RoleAndPermissionsController

--- server/src/test/java/ru/hsHelper/api/RoleAndPermissionsControllerTest.java
+++ server/src/test/java/ru/hsHelper/api/RoleAndPermissionsControllerTest.java
@@ -25,36 +25,69 @@ public class RoleAndPermissionsControllerTest {
     }
 
     @Test
-    public void test1() {
+    public void BasicTest() {
         Map<Permissions.PermissionType, Long> map = new HashMap<>();
         for (Permissions.PermissionType pt : Permissions.PermissionType.values()) {
             map.put(pt, permissionService.createPermission(new Permissions(pt)).getId());
         }
-        Assertions.assertEquals(3, permissionService.getAllPermissions().size());
-        long roleId = roleService.createRole(new Role(Role.RoleType.STUDENT)).getId();
-        roleService.addPermissions(roleId, Set.of(map.get(Permissions.PermissionType.VIEW)));
-        Assertions.assertEquals(3, permissionService.getAllPermissions().size());
-        Assertions.assertEquals(1,
-                roleService.getRoleById(roleId).getPermissions().size());
-        Assertions.assertEquals(1, permissionService.getPermissionById(map.get(Permissions.PermissionType.VIEW)).getRoles().size());
-        roleService.addPermissions(roleId, Set.of(map.get(Permissions.PermissionType.COMMENT), map.get(Permissions.PermissionType.UPDATE)));
-        Assertions.assertEquals(3,
-                roleService.getRoleById(roleId).getPermissions().size());
-        long roleId2 = roleService.createRole(new Role(Role.RoleType.OBSERVER)).getId();
-        roleService.addPermissions(roleId2, Set.of(map.get(Permissions.PermissionType.COMMENT), map.get(Permissions.PermissionType.UPDATE)));
-        Assertions.assertEquals(1, permissionService.getPermissionById(map.get(Permissions.PermissionType.VIEW)).getRoles().size());
-        Assertions.assertEquals(2, permissionService.getPermissionById(map.get(Permissions.PermissionType.UPDATE)).getRoles().size());
-        Assertions.assertEquals(2, permissionService.getPermissionById(map.get(Permissions.PermissionType.COMMENT)).getRoles().size());
-        roleService.deletePermissions(roleId, Set.of(map.get(Permissions.PermissionType.COMMENT)));
-        Assertions.assertEquals(1, permissionService.getPermissionById(map.get(Permissions.PermissionType.VIEW)).getRoles().size());
-        Assertions.assertEquals(2, permissionService.getPermissionById(map.get(Permissions.PermissionType.UPDATE)).getRoles().size());
-        Assertions.assertEquals(1, permissionService.getPermissionById(map.get(Permissions.PermissionType.COMMENT)).getRoles().size());
-        /*roleService.deleteRole(roleId);
+
+        final int permissionCount = 3;
+        int permissionCountInFirstRole = 0;
+        int permissionCountInSecondRole = 0;
+        int[] roleCountInPermissions = new int[3];
+
+        Assertions.assertEquals(permissionCount, permissionService.getAllPermissions().size());
+
+        long FirstRoleId = roleService.createRole(new Role(Role.RoleType.STUDENT)).getId();
+
+        roleService.addPermissions(FirstRoleId, Set.of(map.get(Permissions.PermissionType.VIEW)));
+        permissionCountInFirstRole = 1;
+        roleCountInPermissions[0] = 1;
+        Assertions.assertEquals(permissionCount, permissionService.getAllPermissions().size());
+        Assertions.assertEquals(permissionCountInFirstRole,
+                roleService.getRoleById(FirstRoleId).getPermissions().size());
+        Assertions.assertEquals(roleCountInPermissions[0],
+                permissionService.getPermissionById(map.get(Permissions.PermissionType.VIEW)).getRoles().size());
+
+        roleService.addPermissions(FirstRoleId, Set.of(map.get(Permissions.PermissionType.COMMENT),
+                map.get(Permissions.PermissionType.UPDATE)));
+        permissionCountInFirstRole = 3;
+        roleCountInPermissions[1] = roleCountInPermissions[2] = 1;
+        Assertions.assertEquals(permissionCountInFirstRole,
+                roleService.getRoleById(FirstRoleId).getPermissions().size());
+
+        long SecondRoleId2 = roleService.createRole(new Role(Role.RoleType.OBSERVER)).getId();
+
+        roleService.addPermissions(SecondRoleId2, Set.of(map.get(Permissions.PermissionType.COMMENT),
+                map.get(Permissions.PermissionType.UPDATE)));
+        permissionCountInSecondRole = 2;
+        roleCountInPermissions[1] = roleCountInPermissions[2] = 2;
+        Assertions.assertEquals(roleCountInPermissions[0],
+                permissionService.getPermissionById(map.get(Permissions.PermissionType.VIEW)).getRoles().size());
+        Assertions.assertEquals(roleCountInPermissions[1],
+                permissionService.getPermissionById(map.get(Permissions.PermissionType.UPDATE)).getRoles().size());
+        Assertions.assertEquals(roleCountInPermissions[2],
+                permissionService.getPermissionById(map.get(Permissions.PermissionType.COMMENT)).getRoles().size());
+
+        roleService.deletePermissions(FirstRoleId, Set.of(map.get(Permissions.PermissionType.COMMENT)));
+        permissionCountInFirstRole = 1;
+        roleCountInPermissions[1] = 1;
+        Assertions.assertEquals(roleCountInPermissions[0],
+                permissionService.getPermissionById(map.get(Permissions.PermissionType.VIEW)).getRoles().size());
+        Assertions.assertEquals(roleCountInPermissions[2],
+                permissionService.getPermissionById(map.get(Permissions.PermissionType.UPDATE)).getRoles().size());
+        Assertions.assertEquals(roleCountInPermissions[1],
+                permissionService.getPermissionById(map.get(Permissions.PermissionType.COMMENT)).getRoles().size());
+        /*roleService.deleteRole(FirstRoleId);
         Assertions.assertEquals(0, permissionService.getPermissionById(map.get(Permissions.PermissionType.VIEW)).getRoles().size());
         Assertions.assertEquals(1, permissionService.getPermissionById(map.get(Permissions.PermissionType.UPDATE)).getRoles().size());
         Assertions.assertEquals(1, permissionService.getPermissionById(map.get(Permissions.PermissionType.COMMENT)).getRoles().size());*/
         permissionService.deletePermission(map.get(Permissions.PermissionType.UPDATE));
-        Assertions.assertEquals(1, roleService.getRoleById(roleId).getPermissions().size());
-        Assertions.assertEquals(1, roleService.getRoleById(roleId2).getPermissions().size());
+        roleCountInPermissions[2] = 0;
+        permissionCountInSecondRole = 1;
+        Assertions.assertEquals(permissionCountInFirstRole,
+                roleService.getRoleById(FirstRoleId).getPermissions().size());
+        Assertions.assertEquals(permissionCountInSecondRole,
+                roleService.getRoleById(SecondRoleId2).getPermissions().size());
     }
 }
