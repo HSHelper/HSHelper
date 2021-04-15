0b72b7da3278d9dcc0eaf37b1d7f2e66c32cb6b4
Atos1337 <victor.samoilov.2002@mail.ru>
2021-04-15 17:20:44 +0300

    Create UserUpdateRequest and UserCreateRequest, add preRemove method for UserGroupRole

--- server/src/main/java/ru/hsHelper/api/entities/UserGroupRole.java
+++ server/src/main/java/ru/hsHelper/api/entities/UserGroupRole.java
@@ -1,13 +1,19 @@
 package ru.hsHelper.api.entities;
 
+import com.sun.istack.Nullable;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.springframework.lang.NonNull;
 import ru.hsHelper.api.keys.UserGroupRoleKey;
 
+import javax.persistence.CascadeType;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
+import javax.persistence.PreRemove;
 import java.util.Set;
 
 @Entity
@@ -15,17 +21,19 @@ public class UserGroupRole {
     @EmbeddedId
     private UserGroupRoleKey id;
 
-    @ManyToOne
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @MapsId("userId")
     @JoinColumn(name = "user_id")
+    @Fetch(FetchMode.JOIN)
     private User user;
 
-    @ManyToOne
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @MapsId("groupId")
     @JoinColumn(name = "group_id")
     private Group group;
 
     @ManyToMany
+    @Fetch(FetchMode.JOIN)
     private Set<Role> roles;
 
     public UserGroupRole(User user, Group group, Set<Role> roles) {
@@ -68,5 +76,11 @@ public class UserGroupRole {
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
+    }
+
+    @PreRemove
+    private void removeUserGroupRoleFromUserAndRole() {
+        group.getUserGroupRoleSet().remove(this);
+        user.getGroups().remove(this);
     }
 }
