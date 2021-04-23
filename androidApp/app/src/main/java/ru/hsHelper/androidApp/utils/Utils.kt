96ed2f36f1a5abe05eca0c9e490c82a118675c02
Roman Venediktov <e2e4b6b7@gmail.com>
2021-04-23 18:20:54 +0300

    Fix Utils.kt

--- androidApp/app/src/main/java/ru/hsHelper/androidApp/utils/Utils.kt
+++ androidApp/app/src/main/java/ru/hsHelper/androidApp/utils/Utils.kt
@@ -1,10 +1,11 @@
-package ru.hsHelper.androidApp
+package ru.hsHelper.androidApp.utils
 
 import android.app.Activity
 import android.util.DisplayMetrics
+import android.os.Build
 
 fun getCurrentWindowWidth(activity: Activity) =
-    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
         activity.windowManager.currentWindowMetrics.bounds.width()
     } else {
         val metrics = DisplayMetrics()
