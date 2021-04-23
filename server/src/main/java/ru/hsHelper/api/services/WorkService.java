c7832ba5291686500ee29d5ddcc05a993d132180
Atos1337 <victor.samoilov.2002@mail.ru>
2021-04-23 20:48:56 +0300

    Implement work update; Implement add and delete users from work

--- server/src/main/java/ru/hsHelper/api/services/WorkService.java
+++ server/src/main/java/ru/hsHelper/api/services/WorkService.java
@@ -2,10 +2,17 @@ package ru.hsHelper.api.services;
 
 import ru.hsHelper.api.entities.Work;
 import ru.hsHelper.api.requests.create.WorkCreateRequest;
+import ru.hsHelper.api.requests.update.WorkUpdateRequest;
+
+import java.util.Map;
+import java.util.Set;
 
 public interface WorkService {
     Work createWork(WorkCreateRequest workCreateRequest);
     Work getWorkById(long id);
+    Work updateWork(long id, WorkUpdateRequest workUpdateRequest);
     void deleteWork(long id);
     void preDeleteWork(Work work);
+    Work addUsers(long workId, Set<Long> userIds, Map<Long, String> solutions);
+    Work deleteUsers(long workId, Set<Long> userIds);
 }
