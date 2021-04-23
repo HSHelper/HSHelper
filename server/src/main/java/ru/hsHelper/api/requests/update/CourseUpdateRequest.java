ac51d149c659b4d6c4bf534924fb028b28dd7932
Atos1337 <victor.samoilov.2002@mail.ru>
2021-04-23 19:55:21 +0300

    Implement update course; Implement add and delete users from course;

---
+++ server/src/main/java/ru/hsHelper/api/requests/update/CourseUpdateRequest.java
@@ -0,0 +1,37 @@
+package ru.hsHelper.api.requests.update;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+public class CourseUpdateRequest implements Serializable {
+    @NotEmpty
+    private String name;
+
+    @NotNull
+    private long partitionId;
+
+    public CourseUpdateRequest() {
+    }
+
+    public CourseUpdateRequest(@NotEmpty String name, @NotNull long partitionId) {
+        this.name = name;
+        this.partitionId = partitionId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPartitionId() {
+        return partitionId;
+    }
+
+    public void setPartitionId(long partitionId) {
+        this.partitionId = partitionId;
+    }
+}
--- server/src/main/java/ru/hsHelper/api/services/CourseService.java
+++ server/src/main/java/ru/hsHelper/api/services/CourseService.java
@@ -2,10 +2,17 @@ package ru.hsHelper.api.services;
 
 import ru.hsHelper.api.entities.Course;
 import ru.hsHelper.api.requests.create.CourseCreateRequest;
+import ru.hsHelper.api.requests.update.CourseUpdateRequest;
+
+import java.util.Map;
+import java.util.Set;
 
 public interface CourseService {
     Course createCourse(CourseCreateRequest courseCreateRequest);
+    Course updateCourse(long id, CourseUpdateRequest courseUpdateRequest);
     Course getCourseById(long id);
     void deleteCourse(long id);
     void preDeleteCourse(Course course);
+    Course addUsers(long courseId, Set<Long> userIds, Map<Long, Set<Long>> roleIds);
+    Course deleteUsers(long courseId, Set<Long> userIds);
 }
