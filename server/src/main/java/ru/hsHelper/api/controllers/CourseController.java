5a23de6fb8f43786c20bd3a4bbcad9cd8245bdf4
Atos1337 <victor.samoilov.2002@mail.ru>
2021-04-23 21:53:49 +0300

    Implement CourseController

---
+++ server/src/main/java/ru/hsHelper/api/controllers/CourseController.java
@@ -0,0 +1,60 @@
+package ru.hsHelper.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.hsHelper.api.entities.Course;
+import ru.hsHelper.api.requests.add.ObjectsWithRoleAddRequest;
+import ru.hsHelper.api.requests.create.CourseCreateRequest;
+import ru.hsHelper.api.requests.update.CourseUpdateRequest;
+import ru.hsHelper.api.services.CourseService;
+
+import javax.validation.Valid;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+    private final CourseService courseService;
+
+    @Autowired
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @PostMapping("/")
+    public Course createCourse(@RequestBody @Valid CourseCreateRequest courseCreateRequest) {
+        return courseService.createCourse(courseCreateRequest);
+    }
+
+    @PutMapping("/{id}")
+    public Course updateCourse(@PathVariable long id, @RequestBody @Valid CourseUpdateRequest courseUpdateRequest) {
+        return courseService.updateCourse(id, courseUpdateRequest);
+    }
+
+    @GetMapping("/{id}")
+    public Course getCourse(@PathVariable long id) {
+        return courseService.getCourseById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCourse(@PathVariable long id) {
+        courseService.deleteCourse(id);
+    }
+
+    @PutMapping("/{id}/users")
+    public Course addUsers(@PathVariable long id, @RequestBody @Valid ObjectsWithRoleAddRequest objectsWithRoleAddRequest) {
+        return courseService.addUsers(id, objectsWithRoleAddRequest.getObjectIds(), objectsWithRoleAddRequest.getRoleIds());
+    }
+
+    @DeleteMapping("/{id}/users")
+    public Course deleteUsers(@PathVariable long id, @RequestBody Set<Long> userIds) {
+        return courseService.deleteUsers(id, userIds);
+    }
+}
