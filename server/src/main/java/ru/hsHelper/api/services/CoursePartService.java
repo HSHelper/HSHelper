4c76721635dd08729dd16bc67b8ad80c636532fb
Atos1337 <victor.samoilov.2002@mail.ru>
2021-04-23 20:34:13 +0300

    Implement coursePart update

--- server/src/main/java/ru/hsHelper/api/services/CoursePartService.java
+++ server/src/main/java/ru/hsHelper/api/services/CoursePartService.java
@@ -2,6 +2,7 @@ package ru.hsHelper.api.services;
 
 import ru.hsHelper.api.entities.CoursePart;
 import ru.hsHelper.api.requests.create.CoursePartCreateRequest;
+import ru.hsHelper.api.requests.update.CoursePartUpdateRequest;
 
 import java.util.Set;
 import java.util.Map;
@@ -9,6 +10,7 @@ import java.util.Map;
 public interface CoursePartService {
     CoursePart createCoursePart(CoursePartCreateRequest coursePartCreateRequest);
     CoursePart getCoursePartById(long id);
+    CoursePart updateCoursePart(long id, CoursePartUpdateRequest coursePartUpdateRequest);
     void deleteCoursePart(long id);
     void preDeleteCoursePart(CoursePart coursePart);
     CoursePart addUsers(long coursePartId, Set<Long> userIds, Map<Long, Set<Long>> roleIds);
