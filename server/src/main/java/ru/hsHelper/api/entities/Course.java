b1cd581f160ee17f0a914f30830876946cdfb23b
Atos1337 <victor.samoilov.2002@mail.ru>
2021-05-13 11:18:33 +0300

    Add nullability annotations to Course and CoursePart

--- server/src/main/java/ru/hsHelper/api/entities/Course.java
+++ server/src/main/java/ru/hsHelper/api/entities/Course.java
@@ -5,6 +5,7 @@ import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,28 +21,39 @@ import java.util.Set;
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
     private long id;
 
+    @NotNull
+    @Column(nullable = false)
     private String name;
 
     @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @JoinColumn(name = "default_partition_id")
     @Fetch(FetchMode.JOIN)
+    @NotNull
+    @Column(nullable = false)
     private Partition defaultPartition;
 
     @JsonIgnore
     @OneToMany(mappedBy = "course", cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @Fetch(FetchMode.SELECT)
+    @NotNull
+    @Column(nullable = false)
     private Set<UserCourseRole> users = new HashSet<>();
 
     @JsonIgnore
     @OneToMany(mappedBy = "course", cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @Fetch(FetchMode.SELECT)
+    @NotNull
+    @Column(nullable = false)
     private Set<CoursePart> courseParts = new HashSet<>();
 
     @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @JoinColumn(name = "group_id")
     @Fetch(FetchMode.JOIN)
+    @NotNull
+    @Column(nullable = false)
     private Group group;
 
     public Course() {
