88434d508960c5b29bef4b7ebc2471f8cad3cb31
Atos1337 <victor.samoilov.2002@mail.ru>
2021-05-13 11:27:04 +0300

    Add nullability annotations to UserCoursePartRole

--- server/src/main/java/ru/hsHelper/api/entities/UserCoursePartRole.java
+++ server/src/main/java/ru/hsHelper/api/entities/UserCoursePartRole.java
@@ -5,6 +5,7 @@ import org.hibernate.annotations.FetchMode;
 import ru.hsHelper.api.keys.UserCoursePartRoleKey;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
@@ -12,28 +13,36 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
+import javax.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 public class UserCoursePartRole {
     @EmbeddedId
+    @NotNull
     UserCoursePartRoleKey id = new UserCoursePartRoleKey();
 
     @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @MapsId("userId")
     @JoinColumn(name = "user_id")
     @Fetch(FetchMode.JOIN)
+    @NotNull
+    @Column(nullable = false)
     private User user;
 
     @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @MapsId("coursePartId")
     @JoinColumn(name = "course_part_id")
     @Fetch(FetchMode.JOIN)
+    @NotNull
+    @Column(nullable = false)
     private CoursePart coursePart;
 
     @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @Fetch(FetchMode.JOIN)
+    @NotNull
+    @Column(nullable = false)
     Set<Role> roles = new HashSet<>();
 
     public UserCoursePartRole() {
