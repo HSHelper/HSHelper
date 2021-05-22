10569b966308dbbb1ed1776676d876f7d9d384b4
Atos1337 <victor.samoilov.2002@mail.ru>
2021-05-22 10:11:59 +0300

    Handle exceptions

---
+++ server/src/main/java/ru/hsHelper/api/exceptions/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,19 @@
+package ru.hsHelper.api.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
+    protected ResponseEntity<Object> handleNotFound(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "Such object not found";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+}
