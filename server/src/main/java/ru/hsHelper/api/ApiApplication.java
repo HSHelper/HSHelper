b2c9a7bdfd8b08236d79d47d3c7d679d7d147bf7
Atos1337 <victor.samoilov.2002@mail.ru>
2021-05-28 18:22:51 +0300

    Fix unbuild by moving service-account.json in resources

--- server/src/main/java/ru/hsHelper/api/ApiApplication.java
+++ server/src/main/java/ru/hsHelper/api/ApiApplication.java
@@ -1,13 +1,44 @@
 package ru.hsHelper.api;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.hsHelper.api.configs.FcmSettings;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class ApiApplication {
 
+    private final FcmSettings fcmSettings;
+
+    @Autowired
+    public ApiApplication(FcmSettings fcmSettings) {
+        this.fcmSettings = fcmSettings;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        System.out.println(fcmSettings.getServiceAccountFile());
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(ApiApplication.class.getResourceAsStream(fcmSettings.getServiceAccountFile()));
+        FirebaseOptions firebaseOptions = FirebaseOptions
+                .builder()
+                .setCredentials(googleCredentials)
+                .build();
+        FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "my-app");
+        return FirebaseMessaging.getInstance(app);
     }
 
 }
