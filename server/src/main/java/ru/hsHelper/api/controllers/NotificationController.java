5af5b55145da772755098ceac1a8b084b588e354
Roman Venediktov <e2e4b6b7@gmail.com>
2021-06-09 12:18:47 +0300

    Fix notification by type request

--- server/src/main/java/ru/hsHelper/api/controllers/NotificationController.java
+++ server/src/main/java/ru/hsHelper/api/controllers/NotificationController.java
@@ -33,8 +33,8 @@ public class NotificationController {
         return notificationService.getNotificationById(id);
     }
 
-    @GetMapping("/permission-types")
-    public Notification getNotificaionByNotificationType(@RequestBody Notification.NotificationType notificationType) {
+    @GetMapping("/permission-types/{notificationType}")
+    public Notification getNotificationByNotificationType(@PathVariable Notification.NotificationType notificationType) {
         return notificationService.getNotificationByNotificationType(notificationType);
     }
 
