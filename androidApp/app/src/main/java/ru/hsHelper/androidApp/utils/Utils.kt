e03580d3614bd3713e29d691988ca55f66412de8
kirill <turovkv@yandex.ru>
2021-06-16 00:14:12 +0300

    Move fun withPermission to Utils

--- androidApp/app/src/main/java/ru/hsHelper/androidApp/utils/Utils.kt
+++ androidApp/app/src/main/java/ru/hsHelper/androidApp/utils/Utils.kt
@@ -1,8 +1,13 @@
 package ru.hsHelper.androidApp.utils
 
 import android.app.Activity
+import android.content.pm.PackageManager
 import android.util.DisplayMetrics
 import android.os.Build
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 fun getCurrentWindowWidth(activity: Activity) =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -13,3 +18,35 @@ fun getCurrentWindowWidth(activity: Activity) =
         activity.windowManager.defaultDisplay.getMetrics(metrics)
         metrics.widthPixels
     }
+
+fun AppCompatActivity.withPermissions(permissions: Array<String>, action: () -> Unit) {
+    val name = this.componentName.className
+    val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            if (!it.values.contains(false)) {
+                Log.i(name, "got permissions")
+                action()
+            } else {
+                Log.i(name, "permissions not granted")
+            }
+        }
+
+    var isGranted = true
+    for (permission in permissions) {
+        if (ContextCompat.checkSelfPermission(this, permission)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            isGranted = false
+        }
+    }
+
+    if (isGranted) {
+        Log.i(name, "got permissions")
+        action()
+    } else {
+        Log.i(name, "ask permissions")
+        requestPermissionLauncher.launch(permissions)
+    }
+}
