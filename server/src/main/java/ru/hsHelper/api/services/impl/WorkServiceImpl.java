2f455a91980f8a1a921fca786159088098c5eb4b
Roman Venediktov <e2e4b6b7@gmail.com>
2021-06-15 03:19:31 +0300

    Refactor notifications

--- server/src/main/java/ru/hsHelper/api/services/impl/WorkServiceImpl.java
+++ server/src/main/java/ru/hsHelper/api/services/impl/WorkServiceImpl.java
@@ -3,31 +3,23 @@ package ru.hsHelper.api.services.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.hsHelper.api.configs.PushNotifyConf;
 import ru.hsHelper.api.entities.CoursePart;
-import ru.hsHelper.api.entities.Notification;
-import ru.hsHelper.api.entities.Role;
 import ru.hsHelper.api.entities.User;
-import ru.hsHelper.api.entities.UserCoursePartRole;
 import ru.hsHelper.api.entities.UserWork;
 import ru.hsHelper.api.entities.Work;
+import ru.hsHelper.api.notifications.PushNotificationService;
 import ru.hsHelper.api.repositories.CoursePartRepository;
-import ru.hsHelper.api.repositories.UserCoursePartRoleRepository;
 import ru.hsHelper.api.repositories.UserRepository;
 import ru.hsHelper.api.repositories.UserWorkRepository;
 import ru.hsHelper.api.repositories.WorkRepository;
 import ru.hsHelper.api.requests.create.WorkCreateRequest;
 import ru.hsHelper.api.requests.update.UserWorkUpdateRequest;
 import ru.hsHelper.api.requests.update.WorkUpdateRequest;
-import ru.hsHelper.api.services.NotificationService;
-import ru.hsHelper.api.services.RoleService;
 import ru.hsHelper.api.services.WorkService;
 import ru.hsHelper.api.services.impl.util.UserWorkService;
-
 import java.util.Date;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class WorkServiceImpl implements WorkService {
@@ -37,45 +29,31 @@ public class WorkServiceImpl implements WorkService {
     private final UserWorkRepository userWorkRepository;
     private final UserRepository userRepository;
     private final UserWorkService userWorkService;
-    private final RoleService roleService;
     private final PushNotificationService pushNotificationService;
-    private final NotificationService notificationService;
-    private final UserCoursePartRoleRepository userCoursePartRoleRepository;
 
     @Autowired
     public WorkServiceImpl(WorkRepository workRepository, CoursePartRepository coursePartRepository,
                            UserWorkRepository userWorkRepository, UserRepository userRepository,
-                           UserWorkService userWorkService,
-                           RoleService roleService, PushNotificationService pushNotificationService,
-                           NotificationService notificationService,
-                           UserCoursePartRoleRepository userCoursePartRoleRepository) {
+                           UserWorkService userWorkService, PushNotificationService pushNotificationService) {
         this.workRepository = workRepository;
         this.coursePartRepository = coursePartRepository;
         this.userWorkRepository = userWorkRepository;
         this.userRepository = userRepository;
         this.userWorkService = userWorkService;
-        this.roleService = roleService;
         this.pushNotificationService = pushNotificationService;
-        this.notificationService = notificationService;
-        this.userCoursePartRoleRepository = userCoursePartRoleRepository;
     }
 
     @Transactional
     @Override
     public Work createWork(WorkCreateRequest workCreateRequest) {
         CoursePart coursePart = coursePartRepository.findById(workCreateRequest.getCoursePartId()).orElseThrow(
-                () -> new IllegalArgumentException("No coursePart with such id")
+            () -> new IllegalArgumentException("No coursePart with such id")
         );
         Work work = workRepository.save(new Work(workCreateRequest.getName(), workCreateRequest.getDescription(),
-                workCreateRequest.getDate(), workCreateRequest.getWeight(), workCreateRequest.getBlock(),
-                workCreateRequest.getWorkType(), coursePart));
+            workCreateRequest.getDate(), workCreateRequest.getWeight(), workCreateRequest.getBlock(),
+            workCreateRequest.getWorkType(), coursePart));
         coursePart.addWork(work);
-        Set<User> users = userCoursePartRoleRepository.findAllByCoursePart(work.getCoursePart()).stream().filter(
-                u -> u.getRoles().contains(roleService.getRoleByRoleType(Role.RoleType.STUDENT))).
-                map(UserCoursePartRole::getUser).collect(Collectors.toSet());
-        pushNotificationService.processNotifications(new PushNotifyConf("New work",
-                "Created new Work: " + work.getName(), Map.of("workId", Long.toString(work.getId()))), users,
-                notificationService.getNotificationByNotificationType(Notification.NotificationType.WORKUPDATE));
+        pushNotificationService.newWork(work);
         return work;
     }
 
@@ -83,7 +61,7 @@ public class WorkServiceImpl implements WorkService {
     @Override
     public Work getWorkById(long id) {
         return workRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("No work with such id")
+            () -> new IllegalArgumentException("No work with such id")
         );
     }
 
@@ -91,33 +69,12 @@ public class WorkServiceImpl implements WorkService {
     @Override
     public Work updateWork(long id, WorkUpdateRequest workUpdateRequest) {
         Work work = getWorkById(id);
-        String body = "Work " + work.getName() + " updated: ";
-        if (work.getBlock() != workUpdateRequest.getBlock()) {
-            body += "block; ";
-        }
+        pushNotificationService.modifyWork(work, workUpdateRequest);
         work.setBlock(workUpdateRequest.getBlock());
-        if (!work.getDeadline().equals(workUpdateRequest.getDate())) {
-            body += "deadline; ";
-        }
         work.setDeadline(workUpdateRequest.getDate());
-        if (!work.getDescription().equals(workUpdateRequest.getDescription())) {
-            body += "description; ";
-        }
         work.setDescription(workUpdateRequest.getDescription());
-        if (work.getWeight() != workUpdateRequest.getWeight()) {
-            body += "weight; ";
-        }
         work.setWeight(workUpdateRequest.getWeight());
-        if (!work.getName().equals(workUpdateRequest.getName())) {
-            body += "name; ";
-        }
         work.setName(workUpdateRequest.getName());
-        Set<User> users = userCoursePartRoleRepository.findAllByCoursePart(work.getCoursePart()).stream().filter(
-                u -> u.getRoles().contains(roleService.getRoleByRoleType(Role.RoleType.STUDENT))).
-                map(UserCoursePartRole::getUser).collect(Collectors.toSet());
-        pushNotificationService.processNotifications(new PushNotifyConf("Updated work",
-                        body, Map.of("workId", Long.toString(work.getId()))), users,
-                notificationService.getNotificationByNotificationType(Notification.NotificationType.WORKUPDATE));
         return work;
     }
 
@@ -125,7 +82,7 @@ public class WorkServiceImpl implements WorkService {
     @Override
     public void deleteWork(long id) {
         Work work = workRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("No work with such id")
+            () -> new IllegalArgumentException("No work with such id")
         );
         preDeleteWork(work);
         workRepository.delete(work);
@@ -148,8 +105,8 @@ public class WorkServiceImpl implements WorkService {
         Work work = getWorkById(workId);
         Set<User> users = userRepository.findAllByIdIn(userIds);
         for (User user : users) {
-            UserWork userWork = userWorkRepository.save(new UserWork(user, work, new Date(),
-                    solutions.get(user.getId())));
+            UserWork userWork =
+                userWorkRepository.save(new UserWork(user, work, new Date(), solutions.get(user.getId())));
             user.addWork(userWork);
             work.addUser(userWork);
         }
--- server/src/main/java/ru/hsHelper/api/services/impl/util/UserWorkService.java
+++ server/src/main/java/ru/hsHelper/api/services/impl/util/UserWorkService.java
@@ -3,74 +3,38 @@ package ru.hsHelper.api.services.impl.util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.hsHelper.api.configs.PushNotifyConf;
-import ru.hsHelper.api.entities.Notification;
-import ru.hsHelper.api.entities.Role;
-import ru.hsHelper.api.entities.User;
-import ru.hsHelper.api.entities.UserCoursePartRole;
 import ru.hsHelper.api.entities.UserWork;
 import ru.hsHelper.api.keys.UserWorkKey;
-import ru.hsHelper.api.repositories.UserCoursePartRoleRepository;
+import ru.hsHelper.api.notifications.PushNotificationService;
 import ru.hsHelper.api.repositories.UserWorkRepository;
 import ru.hsHelper.api.requests.update.UserWorkUpdateRequest;
-import ru.hsHelper.api.services.NotificationService;
-import ru.hsHelper.api.services.RoleService;
-import ru.hsHelper.api.services.impl.PushNotificationService;
-
 import java.util.Date;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.Map;
 
 @Service
 public class UserWorkService {
     private final UserWorkRepository userWorkRepository;
     private final PushNotificationService pushNotificationService;
-    private final RoleService roleService;
-    private final NotificationService notificationService;
-    private final UserCoursePartRoleRepository userCoursePartRoleRepository;
-
 
     @Autowired
-    public UserWorkService(UserWorkRepository userWorkRepository, PushNotificationService pushNotificationService,
-                           RoleService roleService,
-                           NotificationService notificationService,
-                           UserCoursePartRoleRepository userCoursePartRoleRepository) {
+    public UserWorkService(UserWorkRepository userWorkRepository, PushNotificationService pushNotificationService) {
         this.userWorkRepository = userWorkRepository;
         this.pushNotificationService = pushNotificationService;
-        this.roleService = roleService;
-        this.notificationService = notificationService;
-        this.userCoursePartRoleRepository = userCoursePartRoleRepository;
     }
 
     @Transactional(readOnly = true)
     public UserWork getUserWork(long userId, long workId) {
         return userWorkRepository.findById(new UserWorkKey(userId, workId)).orElseThrow(
-                () -> new IllegalArgumentException("There is not such user with this work")
+            () -> new IllegalArgumentException("There is not such user with this work")
         );
     }
 
     @Transactional
     public UserWork updateUserWork(long userId, long workId, UserWorkUpdateRequest userWorkUpdateRequest) {
         UserWork userWork = getUserWork(userId, workId);
-        String body = "UserWork " + userWork.getWork().getName() + " updated: ";
-        if (!userWork.getSolution().equals(userWorkUpdateRequest.getSolution())) {
-            body += "solution; ";
-        }
+        pushNotificationService.updateUserWork(userWork, userWorkUpdateRequest);
         userWork.setSolution(userWorkUpdateRequest.getSolution());
-        if (userWork.getMark() != userWorkUpdateRequest.getMark()) {
-            body += "mark; ";
-        }
         userWork.setMark(userWorkUpdateRequest.getMark());
         userWork.setSendTime(new Date());
-        Set<User> users = userCoursePartRoleRepository.findAllByCoursePart(userWork.getWork().getCoursePart()).stream().
-                filter(u -> u.getRoles().contains(roleService.getRoleByRoleType(Role.RoleType.TEACHER))).
-                map(UserCoursePartRole::getUser).collect(Collectors.toSet());
-        users.add(userWork.getUser());
-        pushNotificationService.processNotifications(new PushNotifyConf("Updated UserWork", body,
-                Map.of("userId", Long.toString(userWork.getId().getUserId()),
-                        "workId", Long.toString(userWork.getId().getWorkId()))), users,
-                notificationService.getNotificationByNotificationType(Notification.NotificationType.USERWORKUPDATE));
         return userWork;
     }
 }
