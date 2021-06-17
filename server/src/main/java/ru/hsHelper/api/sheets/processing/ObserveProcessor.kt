df73016b88ba3f5bfc9d523614430392e69499d9
Roman Venediktov <e2e4b6b7@gmail.com>
2021-06-17 12:16:34 +0300

    Set max correct expiration of webhook

--- server/src/main/java/ru/hsHelper/api/sheets/processing/ObserveProcessor.kt
+++ server/src/main/java/ru/hsHelper/api/sheets/processing/ObserveProcessor.kt
@@ -5,12 +5,17 @@ import com.google.api.services.drive.model.Channel
 import java.time.Instant
 
 class ObserveProcessor(private val path: String, private val drive: Drive) {
+    companion object {
+        // Max allowed channel duration time in milliseconds (1 week)
+        private const val maxChannelDuration = 604_800_000L
+    }
+
     fun observe(key: Long, sheetId: String) {
         val channel = Channel().apply {
             id = key.toString()
             type = "webhook"
             address = path + key.toString()
-            expiration = Instant.now().epochSecond + 365 * 24 * 60 * 60
+            expiration = Instant.now().toEpochMilli() + maxChannelDuration
         }
         drive.files().watch(sheetId, channel).execute()
     }
